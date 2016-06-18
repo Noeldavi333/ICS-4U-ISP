2b8ee4c5d9c89d0d9c146407dfb88f312d98d7df
David Noel <davyboy44@yahoo.ca>
2016-06-18 12:48:51 -0400

    Source --> Reformat

--- BasicSearch/src/basicsearch/BasicSearch.java
+++ BasicSearch/src/basicsearch/BasicSearch.java
@@ -5,7 +5,6 @@
  */
 package basicsearch;
 
- 
 /**
  *
  * @author David
@@ -18,26 +17,27 @@ public class BasicSearch {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       String searchParameter = "parameter";
-       ArrayList<String> feed = new ArrayList<>();
-       
-       String out = "";
-        if(searchParameter.equals(" ")){
+        String searchParameter = "parameter";
+        ArrayList<String> feed = new ArrayList<>();
+
+        String out = "";
+        if (searchParameter.equals(" ")) {
             //For the non-filtered we don't even need to search
-            for(int i = 0; i < feed.size(); i++){
-                
-                 out += "\n"+ feed.get(i); 
-            }    
-        }else{
+            for (int i = 0; i < feed.size(); i++) {
+
+                out += "\n" + feed.get(i);
+            }
+        } else {
             //There shouldn't be too many delays to look through so linear search is good.
             // Also may have multiple relevant pieces of data
             // not entirely sure on how the rss gets in yet but we are required to have an array/list in the program...
-            for(int i = 0; i < feed.size(); i++){
-                if(feed.get(i).contains(searchParameter)){
-                 out += "\n"+ feed.get(i);
-                 //then we just print out into the text field
+            for (int i = 0; i < feed.size(); i++) {
+                if (feed.get(i).contains(searchParameter)) {
+                    out += "\n" + feed.get(i);
+                    //then we just print out into the text field
                 }
+            }
         }
+
     }
-    
 }
