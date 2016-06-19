16a5ef822626b7fbc7c0d6b24a9e3801d420c0f2
gmoroziuk6 <gmoroziuk6@gmail.com>
2016-06-19 04:55:05 -0400

    uploaded icons, screenshots, in the process of adding a clock to the app, changed app settings to use custom icons

--- FinalApplication/app/src/main/java/com/example/gmoro/finalapplication/MainActivity.java
+++ FinalApplication/app/src/main/java/com/example/gmoro/finalapplication/MainActivity.java
@@ -7,21 +7,32 @@ import android.view.Window;
 import android.widget.TextView;
 
 import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    String searchParameter;
+    String searchParameter,dateTimeOut, dateTimeString;
 
-    TextView listOfRecentItems;
+    TextView listOfRecentItems,dateViewOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        //add the date and time to the application header
+        SimpleDateFormat dateFormat = new SimpleDateFormat("EEE, MMM d  HH:mm");
+
+        dateFormat.getCalendar();
+
+        //convert date to usable format
 
         //load the saved search parameter
 
@@ -30,7 +41,6 @@ public class MainActivity extends AppCompatActivity {
 
         //trying to prevent a crash if the save file is somehow deleted
         try{
-
         Scanner scanner = new Scanner(file);
 
         //avoiding a scanner error if file is blank
@@ -48,12 +58,20 @@ public class MainActivity extends AppCompatActivity {
               and the user will be directed to the setup*/
         }
 
+        //if there has not been a search parameter defined
         if(searchParameter.isEmpty()){
+            //define the intent to load the setup process
             Intent beginSetupProcessIntent = new Intent(this, SetupActivityOne.class);
 
+            //final result for whatever reason the intent needs one
             final int result = 1;
 
+            //start this setup process
             startActivity(beginSetupProcessIntent);
+
+            //this intent will not close off the main activity process, instead it will continue
+            //running in the background. It does not need to close as the user will back in a couple
+            //of minutes.
         }
 
 
