2ea172913ca7a4ff6d9d674c5d1a7e831c8dae06
gmoroziuk6 <gmoroziuk6@gmail.com>
2016-06-22 20:38:40 -0400

    Dan take it from here

---
+++ FinalApplication/app/src/main/java/com/example/gmoro/finalapplication/HandleXML.java
@@ -0,0 +1,7 @@
+package com.example.gmoro.finalapplication;
+
+/**
+ * Created by gmoro on 2016-06-22.
+ */
+public class HandleXML {
+}
---
+++ FinalApplication/app/src/main/java/com/example/gmoro/finalapplication/updateRSSclass.java
@@ -0,0 +1,17 @@
+package com.example.gmoro.finalapplication;
+
+import android.os.AsyncTask;
+
+import java.net.URL;
+
+/**
+ * Created by gmoro on 2016-06-22.
+ */
+class updateRSSclass extends AsyncTask<Void, Void, Void > {
+
+    @Override
+    protected Void doInBackground(Void... params) {
+        return null;
+    }
+}
+
